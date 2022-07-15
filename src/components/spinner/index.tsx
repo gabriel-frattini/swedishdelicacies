f98c4ed8e2abfb7d372d7200a5b874dd932fbb39
gabriel-frattini <gabriele.g.frattini@gmail.com>
2022-07-15 22:31:08 +0200

    fix implicit any

--- src/components/spinner/index.tsx
+++ src/components/spinner/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import styles from './progress.module.css';
 
-export function Spinner(props) {
+export function Spinner(props: any) {
   return <span role="progressbar" className={styles.spinner} {...props} />;
 }
