a365fc073b6e4b96e1dfefbdd78011dfeccbd0e8
gabriel-frattini <gabriele.g.frattini@gmail.com>
2022-07-16 11:09:19 +0200

    seo

---
+++ src/components/seo.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import Head from 'next/head';
+
+export function Seo({
+  title = '',
+  description = '',
+  pathname = '',
+  image = '',
+  children = null,
+}) {
+  const siteMetadata = {
+    siteTitle: 'Swedish Delicacies',
+    siteTitleDefault: 'Swedish Delicacies',
+    siteDescription: 'We sell swedish food',
+    siteUrl: 'https://swedishdelicacies.vercel.app',
+    siteImage: '',
+  };
+
+  const { siteTitle, siteTitleDefault, siteUrl, siteDescription, siteImage } =
+    siteMetadata;
+
+  const seo = {
+    title: title || siteTitleDefault,
+    description: description || siteDescription,
+    url: `${siteUrl}${pathname}`,
+    image: `${siteUrl}${image || siteImage}`,
+  };
+
+  return (
+    <Head>
+      <html />
+      <meta name="description" content={seo.description} />
+      <meta name="image" content={seo.image} />
+      <meta property="og:title" content={seo.title} />
+      <meta property="og:url" content={seo.url} />
+      <meta property="og:description" content={seo.description} />
+      <meta property="og:image" content={seo.image} />
+      <meta property="og:type" content="website" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={seo.title} />
+      <meta name="twitter:url" content={seo.url} />
+      <meta name="twitter:description" content={seo.description} />
+      <meta name="twitter:image" content={seo.image} />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+
+      {children}
+    </Head>
+  );
+}
