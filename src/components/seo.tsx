6aa8ff22e823a3162a3254f26fb74ecb00fe4965
gabriel-frattini <gabriele.g.frattini@gmail.com>
2022-07-16 11:14:05 +0200

    a

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -8,27 +8,27 @@ export function Seo({
   image = '',
   children = null,
 }) {
-  const siteMetadata = {
-    siteTitle: 'Swedish Delicacies',
-    siteTitleDefault: 'Swedish Delicacies',
-    siteDescription: 'We sell swedish food',
-    siteUrl: 'https://swedishdelicacies.vercel.app',
-    siteImage: '',
-  };
+  // const siteMetadata = {
+  //   siteTitle: 'Swedish Delicacies',
+  //   siteTitleDefault: 'Swedish Delicacies',
+  //   siteDescription: 'We sell swedish food',
+  //   siteUrl: 'https://swedishdelicacies.vercel.app',
+  //   siteImage: '',
+  // };
 
-  const { siteTitle, siteTitleDefault, siteUrl, siteDescription, siteImage } =
-    siteMetadata;
+  // const { siteTitle, siteTitleDefault, siteUrl, siteDescription, siteImage } =
+  //   siteMetadata;
 
-  const seo = {
-    title: title || siteTitleDefault,
-    description: description || siteDescription,
-    url: `${siteUrl}${pathname}`,
-    image: `${siteUrl}${image || siteImage}`,
-  };
+  // const seo = {
+  //   title: title || siteTitleDefault,
+  //   description: description || siteDescription,
+  //   url: `${siteUrl}${pathname}`,
+  //   image: `${siteUrl}${image || siteImage}`,
+  // };
 
   return (
     <Head>
-      <html />
+      {/* <html />
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
       <meta property="og:title" content={seo.title} />
@@ -59,7 +59,7 @@ export function Seo({
         href="/apple-touch-icon.png"
       />
 
-      {children}
+      {children} */}
     </Head>
   );
 }
